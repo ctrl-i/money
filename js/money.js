f84e0bc0cbe36e0768d51d588b48fb9518581045
cmharper <gitlab@cmharper.com>
2020-03-18 17:18:41 +0000

    Fix for lowest ever price

--- js/money.js
+++ js/money.js
@@ -479,7 +479,7 @@ function go(result) {
 	var since = get_since();
 	var since_when = ["low", "high"];
 	for (var lh in since_when){
-		if(since[lh]==""){
+		if(since[lh][0]==""){
 			since[lh][0] = "today";
 			since[lh][1] = data.current[1].toFixed(4);
 		};
